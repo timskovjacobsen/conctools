520e479cbf555c8ebc48b17a4889956f2f787a16
Tim Skov Jacobsen <timskovjacobsen@gmail.com>
2020-01-19 22:52:10 +0100

    Add/modify testing modules

--- tests/test_conctools.py
+++ tests/test_conctools.py
@@ -1,24 +1,104 @@
 #!/usr/bin/env python
 
-"""Tests for `conctools` package."""
+"""
+Tests for `conctools` package.
 
+References used for tests
+    [1]: Reinforced Concrete Design to Eurocode 2, 7th Edition
+            Bill Mosley, John Bungey and Ray Hulse
+"""
+
+# Standard library imports
+import sys
+import os
+
+# Third party imports
 import pytest
+import numpy as np
+from numpy.testing import assert_almost_equal
+from numpy.testing import assert_array_almost_equal
 
+# Get project root directory
+root = os.path.abspath('.')
 
-from conctools import conctools
+# Insert directory for module to test into path
+sys.path.insert(0, f'{root}\\conctools')
+
+# Import module to test and other project specific imports
+from conctools import Section       # noqa: 402
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
+def ref1_example_4_10():
+    '''
+    Fixture for example 4.10 in [1].
+    '''
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    x = [0, 0, 350, 350]
+    y = [0, 450, 450]
+
+    fck = 25
+    fyk = 500
+
+    # Rebar coordinates and diameters
+    xs = [60, 290, 60, 290]
+    ys = [60, 60, 390, 390]
+    ds = [25, 25, 32, 32]
+
+    # Neutral axis locations (Note: in the example it is measured from the top of
+    # the section, but here it's converted to be from the bottom)
+    na = [450-60, 450-158, 450-241, 60, 0, -np.Infinity]
+
+    # Normal force and moment capacities
+    N = [189, -899, 1229, 2248, 2580, 3361]
+    M = [121, 275, 292, 192, 146, 0]
+
+    return x, y, fck, fyk, xs, ys, ds, na, N, M
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_capacity_diagram_ref1_example_4_10(ref1_example_4_10):
+    '''  '''
+    #
+    # ----- Setup --------
+    # Get values from fixture for example 4.10
+    x, y, fck, fyk, xs, ys, ds, na, N, M = ref1_example_4_10
+
+    # Initiate class instance
+    section = Section(vertices=[x, y], rebars=[xs, ys, ds], fck=fck, fyk=fyk)
+
+    desired = (N, M)
+
+    # ----- Exercise -----
+    actual = section.capacity_diagram()
+
+    # ----- Verify -------
+    assert_array_almost_equal(desired, actual)
+
+
+# def test_elastic_centroid(self):
+#     '''TODO'''
+#     pass
+
+
+def test_plastic_centroid():
+    '''Test against example 4.10 from ref. [1] above.'''
+    # ----- Setup --------
+    x = [0, 350, 350, 0]
+    y = [0, 0, 450, 450]
+    xs = [60, 290, 60, 290]
+    ys = [60, 60, 390, 390]
+    ds = [25, 25, 32, 32]
+    fck, fyk = 25, 500
+    alpha_cc = 0.85
+
+    # Create a cross section instance based on input from example
+    section = Section(vertices=[x, y], rebars=[xs, ys, ds], fck=fck, fyk=fyk,
+                      gamma_c=1.5, gamma_s=1.15, alpha_cc=alpha_cc)
+
+    desired = (175, 238)
+
+    # ----- Exercise -----
+    actual = section.plastic_centroid
+
+    # ----- Verify -------
+    assert_almost_equal(desired, actual, decimal=0)
---
+++ tests/test_geometry.py
@@ -0,0 +1,87 @@
+
+"""Tests for `geometry` module."""
+
+# Standard library imports
+import os
+import sys
+
+# Third party imports
+import pytest
+import numpy as np
+from numpy.testing import assert_almost_equal, assert_array_almost_equal
+from shapely.geometry import Point
+from shapely.geometry import Polygon
+
+# Get project root directory
+root = os.path.abspath('..')
+
+# Insert directory for module to test into path
+sys.path.insert(0, f'{root}\\conctools\\conctools')
+
+# Import module to test
+import geometry as gm   # noqa: 402
+
+
+def test_evaluate_points_1():
+
+    # ----- Setup -----
+    x = np.array([0, 1, 2, 3, 4])
+    y = np.array([-3, 0, 3, 6, 9])
+    angle, y_intersect = 0, 2
+
+    desired = np.array([False, False, True, True, True])
+
+    # ----- Exercise -----
+    actual = gm.evaluate_points(x, y, angle, y_intersect)
+
+    # ----- Verify -----
+    assert_array_almost_equal(actual, desired)
+
+
+def test_evaluate_points_negative():
+    # ----- Setup -----
+    x = np.array([175, 175])
+    y = np.array([-30, -255])
+    angle, y_intersect = 0, -60
+
+    desired = np.array([True, False])
+
+    # ----- Exercise -----
+    actual = gm.evaluate_points(x, y, angle, y_intersect)
+
+    # ----- Verify -----
+    assert_array_almost_equal(actural, desired)
+
+
+
+def test_points_in_polygon_rectangle():
+
+    # ----- Setup -----
+    x = [0, 0, 250, 250]
+    y = [0, 300, 300, 0]
+    xs = [40, 125, 210, 40, 210]
+    ys = [40, 40, 40, 460, 460]
+
+    # Create a shapely polygon from cross section vertices
+    polygon = Polygon([(xi, yi) for xi, yi in zip(x, y)])
+
+    # Create a list of shapely points from rebar coordinates
+    points = [Point(xr, yr) for xr, yr in zip(xs, ys)]
+
+    desired = np.array([True, True, True, False, False])
+
+    # ----- Exercise -----
+    actual = gm.points_in_polygon(points=points, polygon=polygon)
+
+    # ----- Verify -----
+    assert_array_almost_equal(actual, desired)
+
+
+def test_create_line():
+    pass
+
+
+def translate_line():
+    '''
+    '''
+    pass

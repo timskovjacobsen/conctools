43edd9374fe21ad256b5df3df5a394572088226f
Tim Skov Jacobsen <timskovjacobsen@gmail.com>
2020-02-23 15:27:31 +0100

    Inilitialize API namespace in __init__.py

--- conctools/__init__.py
+++ conctools/__init__.py
@@ -1,5 +1,27 @@
-"""Top-level package for conctools."""
+'''Top-level package for conctools.
 
-__author__ = """Tim Skov Jacobsen"""
+
+Explanation of the import statements below
+------------------------------------------
+
+*   The purpose of the `from .module_name import *` statements is to import the
+    namespace of the modules at import on user side.
+    E.g.
+
+    Alternative 1:
+        >>> import conctools
+        >>> conctools.Section(...)
+
+    Alternative 2:
+        >>> from conctools.conctools import Section
+        >>> Section(...)
+
+'''
+
+__author__ = '''Tim Skov Jacobsen'''
 __email__ = 'timskovjacobsen@gmail.com'
 __version__ = '0.1.0'
+
+# Import entire namespace from module conctools.py
+from .section import *              # noqa
+from .sectiongen import *           # noqa
